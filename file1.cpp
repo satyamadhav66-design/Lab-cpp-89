4fe6fd34e01c4d7098706c59a631e2c8cd27d6e6
satyamadhav66-design <satyamadhav66@gmail.com>
2026-07-21 09:46:55 +0000

    filespace

---
+++ file1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+namespace SYCS4
+{
+    int add()
+    {
+        int a,b,sum;
+        std::cout<<"Enter two numbers:";
+        std::cin>>a>>b;
+        std::cout<<"sum of two numbers is:"<<(a+b);
+        return 0;
+    }
+}
