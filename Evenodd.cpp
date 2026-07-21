81c0c68054b9242a6206731eeab1e9178e6c2966
satyamadhav66-design <satyamadhav66@gmail.com>
2026-07-21 17:32:42 +0000

    commit

---
+++ Evenodd.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a number:";
+    cin>>n;
+    if(n%2==0)
+    cout<<"Even Number";
+    if(n%2==1)
+    cout<<"Odd Number";
+}
